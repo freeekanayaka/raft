01eb33d5697b9c6a1fc0522aa5b7e686fa5393e9
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-08-07 09:25:56 +0200

    snapshot.c

--- src/snapshot.c
+++ src/snapshot.c
@@ -18,12 +18,8 @@
 
 void snapshotClose(struct raft_snapshot *s)
 {
-    unsigned i;
-    configurationClose(&s->configuration);
-    for (i = 0; i < s->n_bufs; i++) {
-        raft_free(s->bufs[i].base);
-    }
-    raft_free(s->bufs);
+    /* configurationClose(&s->configuration); */
+    raft_free(s->data.base);
 }
 
 void snapshotDestroy(struct raft_snapshot *s)
@@ -32,19 +28,8 @@ void snapshotDestroy(struct raft_snapshot *s)
     raft_free(s);
 }
 
-int snapshotRestore(struct raft *r, struct raft_snapshot *snapshot)
+void snapshotRestore(struct raft *r, struct raft_snapshot *snapshot)
 {
-    int rv;
-
-    assert(snapshot->n_bufs == 1);
-
-    rv = r->fsm->restore(r->fsm, &snapshot->bufs[0]);
-    if (rv != 0) {
-        tracef("restore snapshot %llu: %s", snapshot->index,
-               errCodeToString(rv));
-        return rv;
-    }
-
     configurationClose(&r->configuration);
     r->configuration = snapshot->configuration;
     r->configuration_index = snapshot->configuration_index;
@@ -53,52 +38,7 @@ int snapshotRestore(struct raft *r, struct raft_snapshot *snapshot)
     r->last_applied = snapshot->index;
     r->last_stored = snapshot->index;
 
-    /* Don't free the snapshot data buffer, as ownership has been trasfered to
-     * the fsm. */
-    raft_free(snapshot->bufs);
-
-    return 0;
-}
-
-int snapshotCopy(const struct raft_snapshot *src, struct raft_snapshot *dst)
-{
-    int rv;
-    unsigned i;
-    size_t size;
-    uint8_t *cursor;
-
-    dst->term = src->term;
-    dst->index = src->index;
-
-    rv = configurationCopy(&src->configuration, &dst->configuration);
-    if (rv != 0) {
-        return rv;
-    }
-
-    size = 0;
-    for (i = 0; i < src->n_bufs; i++) {
-        size += src->bufs[i].len;
-    }
-
-    dst->bufs = raft_malloc(sizeof *dst->bufs);
-    assert(dst->bufs != NULL);
-
-    dst->bufs[0].base = raft_malloc(size);
-    dst->bufs[0].len = size;
-    if (dst->bufs[0].base == NULL) {
-        return RAFT_NOMEM;
-    }
-
-    cursor = dst->bufs[0].base;
-
-    for (i = 0; i < src->n_bufs; i++) {
-        memcpy(cursor, src->bufs[i].base, src->bufs[i].len);
-        cursor += src->bufs[i].len;
-    }
-
-    dst->n_bufs = 1;
-
-    return 0;
+    r->snapshot.data = snapshot->data;
 }
 
 #undef tracef
--- src/snapshot.h
+++ src/snapshot.h
@@ -14,15 +14,7 @@ void snapshotDestroy(struct raft_snapshot *s);
  * This will reset the current state of the server as if the last entry
  * contained in the snapshot had just been persisted, committed and applied.
  *
- * The in-memory log must be empty when calling this function.
- *
- * If no error occurs, the memory of the snapshot object gets released. */
-int snapshotRestore(struct raft *r, struct raft_snapshot *snapshot);
-
-/* Make a full deep copy of a snapshot object.
- *
- * All data buffers in the source snapshot will be compacted in a single buffer
- * in the destination snapshot. */
-int snapshotCopy(const struct raft_snapshot *src, struct raft_snapshot *dst);
+ * The in-memory log must be empty when calling this function. */
+void snapshotRestore(struct raft *r, struct raft_snapshot *snapshot);
 
 #endif /* RAFT_SNAPSHOT_H */
