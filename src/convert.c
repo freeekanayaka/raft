c3db6df8acfee732f8365e3fd31e052ff2be8d10
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-08-07 10:02:05 +0200

    convert.c

--- src/convert.c
+++ src/convert.c
@@ -3,18 +3,16 @@
 #include "assert.h"
 #include "configuration.h"
 #include "election.h"
+#include "heap.h"
 #include "log.h"
 #include "membership.h"
 #include "progress.h"
 #include "queue.h"
 #include "request.h"
+#include "tracing.h"
 
 /* Set to 1 to enable tracing. */
-#if 0
-#define tracef(...) Tracef(r->tracer, __VA_ARGS__)
-#else
-#define tracef(...)
-#endif
+#define tracef(...) Tracef(r->tracer, "  " __VA_ARGS__)
 
 /* Convenience for setting a new state value and asserting that the transition
  * is valid. */
@@ -42,6 +40,7 @@ static void convertClearFollower(struct raft *r)
         raft_free(r->follower_state.current_leader.address);
     }
     r->follower_state.current_leader.address = NULL;
+    r->follower_state.current_leader.commit_index = 0;
 }
 
 /* Clear candidate state. */
@@ -53,27 +52,6 @@ static void convertClearCandidate(struct raft *r)
     }
 }
 
-static void convertFailApply(struct raft_apply *req)
-{
-    if (req != NULL && req->cb != NULL) {
-        req->cb(req, RAFT_LEADERSHIPLOST, NULL);
-    }
-}
-
-static void convertFailBarrier(struct raft_barrier *req)
-{
-    if (req != NULL && req->cb != NULL) {
-        req->cb(req, RAFT_LEADERSHIPLOST);
-    }
-}
-
-static void convertFailChange(struct raft_change *req)
-{
-    if (req != NULL && req->cb != NULL) {
-        req->cb(req, RAFT_LEADERSHIPLOST);
-    }
-}
-
 /* Clear leader state. */
 static void convertClearLeader(struct raft *r)
 {
@@ -82,29 +60,12 @@ static void convertClearLeader(struct raft *r)
         r->leader_state.progress = NULL;
     }
 
-    /* Fail all outstanding requests */
-    while (!QUEUE_IS_EMPTY(&r->leader_state.requests)) {
-        struct request *req;
-        queue *head;
-        head = QUEUE_HEAD(&r->leader_state.requests);
-        QUEUE_REMOVE(head);
-        req = QUEUE_DATA(head, struct request, queue);
-        assert(req->type == RAFT_COMMAND || req->type == RAFT_BARRIER);
-        switch (req->type) {
-            case RAFT_COMMAND:
-                convertFailApply((struct raft_apply *)req);
-                break;
-            case RAFT_BARRIER:
-                convertFailBarrier((struct raft_barrier *)req);
-                break;
-        };
-    }
-
     /* Fail any promote request that is still outstanding because the server is
      * still catching up and no entry was submitted. */
-    if (r->leader_state.change != NULL) {
+    if (r->leader_state.promotee_id != 0) {
+        /*
         convertFailChange(r->leader_state.change);
-        r->leader_state.change = NULL;
+        */
     }
 }
 
@@ -136,6 +97,7 @@ void convertToFollower(struct raft *r)
 
     r->follower_state.current_leader.id = 0;
     r->follower_state.current_leader.address = NULL;
+    r->follower_state.current_leader.commit_index = 0;
 }
 
 int convertToCandidate(struct raft *r, bool disrupt_leader)
@@ -150,7 +112,7 @@ int convertToCandidate(struct raft *r, bool disrupt_leader)
     convertSetState(r, RAFT_CANDIDATE);
 
     /* Allocate the votes array. */
-    r->candidate_state.votes = raft_malloc(n_voters * sizeof(bool));
+    r->candidate_state.votes = HeapMalloc(n_voters * sizeof(bool));
     if (r->candidate_state.votes == NULL) {
         return RAFT_NOMEM;
     }
@@ -187,7 +149,7 @@ int convertToLeader(struct raft *r)
     convertSetState(r, RAFT_LEADER);
 
     /* Reset timers */
-    r->election_timer_start = r->io->time(r->io);
+    r->election_timer_start = r->clock->now(r->clock);
 
     /* Reset apply requests queue */
     QUEUE_INIT(&r->leader_state.requests);
@@ -197,8 +159,6 @@ int convertToLeader(struct raft *r)
     if (rv != 0) {
         return rv;
     }
-
-    r->leader_state.change = NULL;
 
     /* Reset promotion state. */
     r->leader_state.promotee_id = 0;
@@ -212,7 +172,8 @@ int convertToLeader(struct raft *r)
 void convertToUnavailable(struct raft *r)
 {
     /* Abort any pending leadership transfer request. */
-    if (r->transfer != NULL) {
+    if (r->transfer.id != 0) {
+        tracef("transfer to %llu -> cancel", r->transfer.id);
         membershipLeadershipTransferClose(r);
     }
     convertClear(r);
