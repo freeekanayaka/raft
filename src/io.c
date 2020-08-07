3e843f45c7c78f6dbbd7059ee48c5b323589c71e
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-08-07 10:35:25 +0200

    io.c

---
+++ src/io.c
@@ -0,0 +1,60 @@
+#include "io.h"
+
+int ioPersistTermAndVote(struct raft *r, raft_term term, raft_id voted_for)
+{
+    struct raft_persist_term_and_vote *io;
+    io = HeapMalloc(sizeof *io);
+    if (io == NULL) {
+        return RAFT_NOMEM;
+    }
+
+    io->type = RAFT_PERSIST_TERM_AND_VOTE;
+    io->term = term;
+    io->voted_for = voted_for;
+
+    QUEUE_PUSH(&r->io, &io->queue);
+
+    return 0;
+}
+
+int ioPersistEntries(struct raft *r,
+                     raft_index first_index,
+                     struct raft_entry *entries,
+                     unsigned n)
+{
+    struct raft_persist_entries *io;
+    io = HeapMalloc(sizeof *io);
+    if (io == NULL) {
+        return RAFT_NOMEM;
+    }
+
+    io->type = RAFT_PERSIST_ENTRIES;
+    io->first_index = first_index;
+    io->entries = entries;
+    io->n = n;
+
+    QUEUE_PUSH(&r->io, &io->queue);
+
+    return 0;
+}
+
+int ioSendMessage(struct raft *r,
+                  raft_id id,
+                  const char *address,
+                  struct raft_message message)
+{
+    struct raft_send_message *io;
+    io = HeapMalloc(sizeof *io);
+    if (io == NULL) {
+        return RAFT_NOMEM;
+    }
+
+    io->type = RAFT_SEND_MESSAGE;
+    io->id = id;
+    io->address = address;
+    io->message = message;
+
+    QUEUE_PUSH(&r->io, &io->queue);
+
+    return 0;
+}
---
+++ src/io.h
@@ -0,0 +1,23 @@
+/* Enqueue I/O requests. */
+
+#ifndef IO_H_
+#define IO_H_
+
+#include "../include/raft.h"
+
+#include "heap.h"
+#include "queue.h"
+
+int ioPersistTermAndVote(struct raft *r, raft_term term, raft_id voted_for);
+
+int ioPersistEntries(struct raft *r,
+                     raft_index first_index,
+                     struct raft_entry *entries,
+                     unsigned n);
+
+int ioSendMessage(struct raft *r,
+                  raft_id id,
+                  const char *address,
+                  struct raft_message message);
+
+#endif /* IO_H_ */
