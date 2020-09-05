aa0f391b1b8e0a6f37c9f6b8aa107b03eebb5e29
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-09-05 19:11:18 +0200

    entry.c

--- src/entry.h
+++ src/entry.h
@@ -7,13 +7,4 @@
  * itself. The entries are supposed to belong to one or more batches. */
 void entryBatchesDestroy(struct raft_entry *entries, size_t n);
 
-/* Create a copy of a log entry, including its data. */
-int entryCopy(const struct raft_entry *src, struct raft_entry *dst);
-
-/* Create a single batch of entries containing a copy of the given entries,
- * including their data. */
-int entryBatchCopy(const struct raft_entry *src,
-                   struct raft_entry **dst,
-                   size_t n);
-
 #endif /* ENTRY_H */
